4da2f9d77c4860111dabb0b9569810543cd8214e
Egemen Agustos <info@egemenagustos.com>
2024-06-21 20:08:46 +0300

    coding-the-quartz-job-and-demonstrating-outbox-behavior

--- Order.Outbox.Table.Publisher/Jobs/OrderOutboxPublishJob.cs
+++ Order.Outbox.Table.Publisher/Jobs/OrderOutboxPublishJob.cs
@@ -1,6 +1,8 @@
 ﻿using MassTransit;
 using Order.Outbox.Table.Publisher.Entities;
 using Quartz;
+using Shared.Events;
+using System.Text.Json;
 
 namespace Order.Outbox.Table.Publisher.Jobs
 {
@@ -13,6 +15,24 @@ namespace Order.Outbox.Table.Publisher.Jobs
                 OrderOutboxSingletonDatabase.DataReaderBusy();
 
                 List<OrderOutbox> orderOutboxes = (await OrderOutboxSingletonDatabase.QueryAsync<OrderOutbox>($@"SELECT * FROM OrderOutboxes WHERE PROCESSEDDATE IS NULL ORDER BY OCCUREDON ASC")).ToList();
+
+                foreach(var orderOutbox in orderOutboxes)
+                {
+                    if(orderOutbox.Type == nameof(OrderCreatedEvent))
+                    {
+                        OrderCreatedEvent orderCreatedEvent = JsonSerializer.Deserialize<OrderCreatedEvent>(orderOutbox.Payload);
+
+                        if(orderCreatedEvent is not null)
+                        {
+                            await publishEndpoint.Publish(orderCreatedEvent);
+                            await OrderOutboxSingletonDatabase
+                                .ExecuteAsync($"UPDATE OrderOutboxes SET PROCESSEDDATE= GETDATE() WHERE ID ='{orderOutbox.Id}'");
+                        }
+                    }
+                }
+
+                OrderOutboxSingletonDatabase.DataReaderReady();
+                Console.WriteLine("Order outbox table checked!");
             }
         }
     }
