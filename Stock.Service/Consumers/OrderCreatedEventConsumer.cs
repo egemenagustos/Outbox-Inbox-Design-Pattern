c7451726285cf84ff8f296bd8eb544062343567f
Egemen Agustos <info@egemenagustos.com>
2024-06-21 20:56:37 +0300

    adding-inbox-design-pattern

--- Stock.Service/Consumers/OrderCreatedEventConsumer.cs
+++ Stock.Service/Consumers/OrderCreatedEventConsumer.cs
@@ -1,14 +1,42 @@
 ﻿using MassTransit;
 using Shared.Events;
+using Stock.Service.Models.Contexts;
+using Stock.Service.Models.Entities;
 using System.Text.Json;
 
 namespace Stock.Service.Consumers
 {
-    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    public class OrderCreatedEventConsumer(StockDbContext stockDbContext) : IConsumer<OrderCreatedEvent>
     {
         public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
         {
-            Console.WriteLine(JsonSerializer.Serialize(context.Message));
+            await stockDbContext.OrderInboxes.AddAsync(new()
+            {
+                Processed = false,
+                Payload = JsonSerializer.Serialize(context.Message)
+            });
+
+            await stockDbContext.SaveChangesAsync();
+
+            List<OrderInbox> orderInboxes = stockDbContext.OrderInboxes
+                                                            .Where(x => !x.Processed)
+                                                            .ToList();
+
+            foreach(var orderInbox in orderInboxes)
+            {
+                OrderCreatedEvent orderCreatedEvent = JsonSerializer.Deserialize<OrderCreatedEvent>(orderInbox.Payload);
+
+                Console.WriteLine($"{orderCreatedEvent.OrderId} - Stock processing of the order completed!");
+
+                orderInbox.Processed = true;
+                /* 
+                SaveChanges metodunu forech dışında çağırmak burada çok doğru olmayacaktır.
+                Sebebi ise döngüde 100 datada döndük 99 tanesi de işledik ama 100. datada hata aldık varsayalım.
+                O zaman hangi data da hata olduğunu bulmamamız epey yorucu olabilir ve mantık olarak da
+                çok doğru bir yaklaşım olmayacaktır.
+                */
+                await stockDbContext.SaveChangesAsync();
+            }
         }
     }
 }
--- Stock.Service/Program.cs
+++ Stock.Service/Program.cs
@@ -1,6 +1,8 @@
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using Shared;
 using Stock.Service.Consumers;
+using Stock.Service.Models.Contexts;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -13,6 +15,11 @@ builder.Services.AddMassTransit(configurator =>
 
         _configure.ReceiveEndpoint(RabbitMQSettings.Stock_OrderCreatedEvent, e => e.ConfigureConsumer<OrderCreatedEventConsumer>(context));
     });
+});
+
+builder.Services.AddDbContext<StockDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["Mssql"]);
 });
 
 var host = builder.Build();
