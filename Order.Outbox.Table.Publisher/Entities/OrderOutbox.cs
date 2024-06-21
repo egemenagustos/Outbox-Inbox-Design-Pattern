da96cdb6566cf3bd02094b8261aebd9d584d101b
Egemen Agustos <info@egemenagustos.com>
2024-06-21 19:53:51 +0300

    creating-the-dapper-operational-class

---
+++ Order.Outbox.Table.Publisher/Entities/OrderOutbox.cs
@@ -0,0 +1,17 @@
+﻿namespace Order.Outbox.Table.Publisher.Entities
+{
+    public class OrderOutbox
+    {
+        public int Id { get; set; }
+
+        public DateTime OccuredOn { get; set; }
+
+        public DateTime? ProcessedDate { get; set; }
+
+        //Hangi event olacak...
+        public string Type { get; set; }
+
+        //Gönderilecek event bilgileri...
+        public string Payload { get; set; }
+    }
+}
